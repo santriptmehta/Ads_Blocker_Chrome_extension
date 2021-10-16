97ba10c1d700ab0dbae5b80d3b5b41aa8916b582
Santript Mehta <santriptmehta@gmail.com>
2021-10-16 22:56:54 +0530

    Initiation of Project

---
+++ background.js
@@ -0,0 +1,26 @@
+setInterval(function(){
+    var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+    if(skipButton != undefined && skipButton.length > 0){
+        console.log("Ad detected");
+        skipButton[0].click();
+    }
+},3000)
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details){return {cancel: true};},
+    {urls: defaultFilters},
+    ["blocking"]
+
+)
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
